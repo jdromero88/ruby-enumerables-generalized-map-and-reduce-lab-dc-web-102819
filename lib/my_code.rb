502544a2570d40d04e30b64f7ed8cce2aaaa699d
José Romero <jdromerov88@gmail.com>
2019-10-26 04:51:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,4 +3,4 @@ def map_this(element1, element2)
   yiel("A #{element1} and #{element2} sandwich")
 end
 
-map_this("hola tengo hambre", "quiero comer un burrito"){|variable| "#{b} con pan"}
+map_this("hola tengo hambre", "quiero comer un burrito") {|variable| "#{b} con pan"}
